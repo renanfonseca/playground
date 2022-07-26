5f5ac44288af4b73a08283c3f6d9c236b9ad5914
Renan Fonseca <marcos.renan.cf@gmail.com>
2022-07-26 17:33:13 -0300

    carregamento eager para lazy

--- Programming/java/java_jpa_hibernate/src/main/java/br/com/renanfonseca/java_jpa_hibernate/dao/ProdutoDao.java
+++ Programming/java/java_jpa_hibernate/src/main/java/br/com/renanfonseca/java_jpa_hibernate/dao/ProdutoDao.java
@@ -17,26 +17,23 @@ public class ProdutoDao {
 	public void cadastrar(Produto produto) {
 		this.em.persist(produto);
 	}
-	
+
 	public Produto buscarPorId(Long id) {
 		return em.find(Produto.class, id);
 	}
-	
+
 	public List<Produto> buscarTodos() {
-		
+
 		String jpql = "SELECT p FROM Produto p";
-		
+
 		return em.createQuery(jpql, Produto.class).getResultList();
 	}
-	
-public List<Produto> buscarPorNome(String nome) {
-		
+
+	public List<Produto> buscarPorNome(String nome) {
+
 		String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
-		
-		
-		return em.createQuery(jpql, Produto.class)
-				.setParameter("nome", nome)
-				.getResultList();
+
+		return em.createQuery(jpql, Produto.class).setParameter("nome", nome).getResultList();
 	}
 
 }
