331da63796ec0cb97da74ef1a2e18438886af340
Renan Fonseca <marcos.renan.cf@gmail.com>
2022-07-29 14:00:11 -0300

    criando projeto spring mvc

---
+++ Programming/java/spring_mvc_thymeleaf_bootstrap/src/main/java/br/com/renanfonseca/mvc/spring_mvc_thymeleaf_bootstrap/SpringMvcThymeleafBootstrapApplication.java
@@ -0,0 +1,13 @@
+package br.com.renanfonseca.mvc.spring_mvc_thymeleaf_bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcThymeleafBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcThymeleafBootstrapApplication.class, args);
+	}
+
+}
---
+++ Programming/java/spring_mvc_thymeleaf_bootstrap/src/test/java/br/com/renanfonseca/mvc/spring_mvc_thymeleaf_bootstrap/SpringMvcThymeleafBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.renanfonseca.mvc.spring_mvc_thymeleaf_bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcThymeleafBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
